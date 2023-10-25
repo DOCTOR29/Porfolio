e7cfb0ed7104ad67906ffb7e2ff42b12aabb9129
Doctor <naveedislam545@gmail.com>
2023-10-25 11:12:47 +0530

    socials

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -6,10 +6,14 @@ type Data = {
     socials: Social[],
 }
 const query = groq`*[_type == "social"]`
+const query1 = groq`*[_id == "d92fc2fb-5ec4-4a6d-81c2-6d966f8dff24"]`
 export const fetchSocials = async () => { 
    
-    
-    const data = await client.fetch(query);
+
+    const data = await client.fetch(query,
+        {
+            cache: 'no-store'
+    });
     const socials: Social[] = data
     return socials;
 } 
